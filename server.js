f76761bc75a6603d355ea7662f6d173a27cbde01
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-22 16:42:51 -0400

    create server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function (
+  req, res){
+    res.writeHead(200, {'Content-Type':
+    'text/html'});
+    res.end('<h1>Hello there</h1>');
+  });
+
+server.listen(3000);
