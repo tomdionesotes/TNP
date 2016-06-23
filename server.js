97a719662290784c28fa716f7d0566256ffa06b4
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-23 16:28:52 -0400

    test config var

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require('http');
 
+var shouldshow = process.env.TEST
+
 var server = http.createServer(function (
   req, res){
     res.writeHead(200, {'Content-Type':
     'text/html'});
-    res.end('<h1>Hello there</h1>');
+    res.end('<h1>shouldshow</h1>');
   });
 
 var port = Number(process.env.PORT || 3000);
