491c30313f355d49885c1a5e86d3943748066f0a
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-23 16:40:44 -0400

    alert

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var server = http.createServer(function (
   req, res){
     res.writeHead(200, {'Content-Type':
     'text/html'});
-    res.end('<h1>shouldShowUp</h1>');
+    res.end('<h1>Hello there!</h1>
+    <script type="text/javascript">alert('shouldShowUp')</script>');
   });
 
 var port = Number(process.env.PORT || 3000);
