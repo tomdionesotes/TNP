c7ecdda03ff4e37220f90c74624dffd5b3b950bd
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-23 16:41:41 -0400

    take out h1

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ var server = http.createServer(function (
   req, res){
     res.writeHead(200, {'Content-Type':
     'text/html'});
-    res.end('<h1>Hello there!</h1>
-    <script type="text/javascript">alert('shouldShowUp')</script>');
+    res.end('<script type="text/javascript">alert('shouldShowUp')</script>');
   });
 
 var port = Number(process.env.PORT || 3000);
