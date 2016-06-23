ace7bb36a918c5a22834620a6d358c0b8b2810c5
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-23 16:43:00 -0400

    take out test

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var http = require('http');
 
-var shouldShowUp = process.env.TEST
-
 var server = http.createServer(function (
   req, res){
     res.writeHead(200, {'Content-Type':
     'text/html'});
-    res.end('<script type="text/javascript">alert('shouldShowUp')</script>');
+    res.end('<h1>hi there</h1>');
   });
 
 var port = Number(process.env.PORT || 3000);
