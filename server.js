d7532e0d6f400a2542c27fe3d23771c5931bfd8e
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-23 16:30:42 -0400

    test for heroic automatic deploy

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 
-var shouldshow = process.env.TEST
+var shouldShowUp = process.env.TEST
 
 var server = http.createServer(function (
   req, res){
     res.writeHead(200, {'Content-Type':
     'text/html'});
-    res.end('<h1>shouldshow</h1>');
+    res.end('<h1>shouldShowUp</h1>');
   });
 
 var port = Number(process.env.PORT || 3000);
