31a7a9aa3815b7d9e555eca2ef6be09acb06d628
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-24 11:44:02 -0500

    display home page

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ var todo = mongoose.model('todo', {
   text: String
 });
 
+// front end
+app.get('*', function(req, res) {
+  res.sendfile('./public/index.html');
+});
+
 // listen
 app.listen(8080);
 console.log("App listening on port 8080");
