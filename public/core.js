9cdbfeffd93e1e1eaf74a3186ad9f5dfbaa69547
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-24 12:13:41 -0500

    get and display todos on page load

--- public/core.js
+++ public/core.js
@@ -1,5 +1,20 @@
 var myTodo = angular.module('myTodo', []);
 
-function mainController($scope, $http){
-  
+function mainController($scope, $http) {
+  $scope.formData = {};
+
+  // on page load, get all todos and display
+  $http.get('./api/todos')
+  .success(function(data) {
+    $scope.todos = data;
+    console.log(data);
+  })
+  .error(function (data) {
+    console.log('Error: ' + data);
+  });
+
+  // on todo creation via the form
+
+
+
 };
