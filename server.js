da5a122c4bb4ed6853ba2d957f0c8a333f6373f4
Tom Dionesotes <tdionesotes@ngpvan.com>
2016-06-24 13:16:32 -0500

    old version of sendfile

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var todo = mongoose.model('todo', {
 
 // front end
 app.get('*', function(req, res) {
-  res.sendFile('./public/index.html');
+  res.sendfile('./public/index.html');
 });
 
 // listen
